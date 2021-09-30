9c1e9c6d72a142c2f2dfcd1c588d9e2ea6a73a0c
ankuranurag2 <ankuranurag2@gmail.com>
2021-09-30 10:50:36 +0530

    Changelog: - Font related changes
    Signed-off-by: ankuranurag2 <ankuranurag2@gmail.com>

--- app/src/main/java/dev/ankuranurag2/trendingrepo/presentation/theme/Type.kt
+++ app/src/main/java/dev/ankuranurag2/trendingrepo/presentation/theme/Type.kt
@@ -9,20 +9,29 @@ import androidx.compose.ui.unit.sp
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        color = TextColor
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    body2 = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        color = TextColor
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily.SansSerif,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        letterSpacing = 0.1.sp,
+        color = TextColor
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp,
+        letterSpacing = 0.1.sp,
+        color = TextColor
     )
-    */
 )
